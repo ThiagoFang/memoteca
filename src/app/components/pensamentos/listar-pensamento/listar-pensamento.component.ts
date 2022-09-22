b4efa41722585837879c668c647cd9dee0dd0b38
Thiago <thiagofang@gmail.com>
2022-09-22 14:10:25 -0300

    organizando a comunicação entre componentes

--- src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -7,6 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
+  listaPensamentos = [
+    {
+      conteudo: "Comunicação entre componentes",
+      autoria: "Angular",
+      modelo: "modelo3"
+    },
+    {
+      conteudo: "Binding of Isac lorem Angular",
+      autoria: "Angular",
+      modelo: "modelo2"
+    },
+    {
+      conteudo: "Mussunm Impsum, lorem dolem lorem impsum dolor amet dyingMussunm Impsum, lorem dolem lorem impsum dolor amet dying a light em mindus impsu Mussunm Impsum, lorem dolem lorem impsum dolor amet dying a light em mindus impsu Mussunm Impsum, lorem dolem lorem impsum dolor amet dying a light em mindus impsu",
+      autoria: "Angular",
+      modelo: "modelo2"
+    },
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
